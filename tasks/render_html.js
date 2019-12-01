d425ce44d2c5126e9d04ba9b5a3d5876b2d583dc
Dennis Reimann <mail@dennisreimann.de>
2019-12-01 13:55:26 +0100

    Finish everything but language options

--- tasks/render_html.js
+++ tasks/render_html.js
@@ -9,11 +9,16 @@ const {
 } = require('./util')
 
 // Render html templates
+const langs = getLanguages('en-json')
 const indexTmpl = getTemplate('html/tmpl.html')
 const donateTmpl = getTemplate('html/donate/tmpl.html')
 const footerTmpl = getTemplate('html/footer-tmpl.html')
-
-const langs = getLanguages('en-json')
+const jsonDonations = getTemplate('json/donations.json')
+const donationsBlock = JSON.parse(jsonDonations).map(([name, url, avatar]) => `
+            <a href="${url}" class="ind-icon">
+              <div class="in-img" style="background-image:url(${avatar})"></div>
+              <span class="in-nom">${name}</span>
+            </a>`).join('')
 
 console.log(`HTML: Rendering ${langs.length} translations …`)
 
@@ -21,11 +26,14 @@ langs.forEach(lang => {
   const [lng] = lang.split('_')
   const isRtl = ['he', 'ar'].includes(lng)
   const directory = lng === 'en' ? '' : `${lang}/`
-  const translations = getTransifexJSON(`en-json/translation/${lang}`)
+  const t = getTransifexJSON(`en-json/translation/${lang}`)
 
-  const footerParts = translations[29].split('FLAT18.CO.UK')
+  const footerParts = t[29].split('FLAT18.CO.UK')
 
   const tmplVars = {
+    jsonDonations,
+    donationsBlock,
+    __lang: lng === 'en' ? 'en' : lang,
     directoryPrefix: lng === 'en' ? '' : `/${lang}`,
     to: isRtl ? 'rtl' : 'ltr',
     rl: isRtl ? 'left' : 'right',
@@ -33,26 +41,23 @@ langs.forEach(lang => {
     L_rl: isRtl ? 'r' : 'l',
     R_rl: isRtl ? 'l' : 'r',
     lnstr: lng,
-    lngOpts: 'TODO',
     sub: getLanguageName(lang),
     exp0: lng,
     enlngfb: lng === 'en' ? '' : '<track src="/vtt/en.vtt" label="English" kind="subtitles" srclang="en">',
-
-    _donate: titleCase(translations[38]),
-    _blog: titleCase(translations[2]),
+    _donate: titleCase(t[38]),
+    _blog: titleCase(t[2]),
+    exp5624: t[24] && t[56] ? t[24].split(t[56])[0] : '',
+    exp5756: t[24] && t[56] && t[57] && t[24].split(t[56])[1] ? t[24].split(t[56])[1].split(t[57])[0] : '',
+    exp5857: t[24] && t[57] && t[58] && t[24].split(t[57])[1] ? t[24].split(t[57])[1].split(t[58])[0] : '',
+    exp5069: t[24] && t[59] && t[60] && t[24].split(t[59])[1] ? t[24].split(t[59])[1].split(t[60])[0] : '',
     exp229: footerParts.length > 1 ? footerParts[1].replace('.', '<br>') : '',
     exp29: footerParts[0],
-    j: translations
+    exp71: t[11],
+    j: t,
+    // TODO:
+    // lngOpts:
+    // lDngOptsDonate:
   }
-
-
-    // $exp5624 = isset($j[56], $j[24]) && strpos($j[24], $j[56]) ? explode($j[56], $j[24])[0] : '';
-    // $exp5756 = isset($j[56], $j[57]) && strpos($j[24], $j[56]) ? explode($j[57], explode($j[56], $j[24])[1])[0] : '';
-    // $exp5857 = isset($j[57], $j[58]) && strpos($j[24], $j[57]) ? explode($j[58], explode($j[57], $j[24])[1])[0] : '';
-    // $exp5069 = isset($j[59], $j[60]) && strpos($j[24], $j[59]) ? explode($j[60], explode($j[59], $j[24])[1])[0] : '';
-    // $exp29 = isset($j[29]) ? strpos($j[29], 'FLAT18.CO.UK') ? explode("FLAT18.CO.UK", $j[29])[0] : $j[29] : '';
-    // $exp229 = isset($j[29]) && strpos($j[29], 'FLAT18.CO.UK') ? str_replace('.', '<br>', explode("FLAT18.CO.UK", $j[29])[1]) : '';
-
 
   // render footer and add the result to the vars
   tmplVars.ftblk = replaceTemplateVars(footerTmpl, tmplVars)
